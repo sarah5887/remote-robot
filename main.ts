2fc5979a5c83a86b990190e2480b3016efbf5972
sarah5887 <113192301+sarah5887@users.noreply.github.com>
2022-10-31 10:44:02 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    radio.sendNumber(4)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(3)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(2)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    radio.sendNumber(5)
+})
+basic.showLeds(`
+    # . . . .
+    . # # . .
+    . . # . .
+    . . # # .
+    . . . # .
+    `)
+radio.setGroup(12)
